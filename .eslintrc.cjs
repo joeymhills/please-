db8434f02ba82826e4970e53d7ce5362332be52c
Joey Hills <joeymhills@gmail.com>
2023-06-26 12:48:43 -0400

    emeowo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
