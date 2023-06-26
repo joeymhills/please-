4b0bf39aacebf0eb243e5a940ab53181e9b84f86
Joey Hills <joeymhills@gmail.com>
2023-06-26 14:56:32 -0400

    meow

--- src/components/AccoladeBox.tsx
+++ src/components/AccoladeBox.tsx
@@ -46,7 +46,11 @@ messaging: '', frequency: '', notifDate: '', cmcontact: '', sourceatr: '', where
 
   async function create(data: FormData) { 
     try {
+<<<<<<< HEAD
       await fetch("http://localhost:3000/api/create",{
+=======
+      await fetch('https://please-work-beta.vercel.app/api/create',{
+>>>>>>> d8678dd (meow)
         body: JSON.stringify(data),
         headers: { 'Content-Type': 'Application/json'},
         method: 'POST'});
